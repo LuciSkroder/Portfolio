70185a97f79ddd342ecdf08316904b6da18d8394
LuciSkroder <Luci.skroder@gmail.com>
2025-09-08 15:52:13 +0200

    Personal logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router";
 import LanguageSelector from "./LanguageSelector";
 import { useTranslation } from "react-i18next";
+import FavIcon from "../assets/facicon-l.svg?react";
 
 export default function NavBar() {
   const { t } = useTranslation();
   return (
     <div>
+      <FavIcon className="personal-logo" />
       <nav>
         <NavLink to="/cv">CV</NavLink>
         <NavLink to="/" end>
