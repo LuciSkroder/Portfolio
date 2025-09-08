a5114edc0b5f824d5cae00d75a3f677efdb7679d
LuciSkroder <Luci.skroder@gmail.com>
2025-09-08 09:22:46 +0200

    fix

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import i18n from "../i18n";
 
 export default function LanguageSelector() {
-  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
 
   const chooseLanguage = (e) => {
     e.preventDefault();
-    i18n.changeLanguage(e.target.value);
+    i18n.changeLanguage(e.target.value); // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
     setSelectedLanguage(e.target.value);
+    localStorage.setItem("lang", e.target.value);
   };
 
   return (
