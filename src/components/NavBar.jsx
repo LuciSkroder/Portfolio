1d6cf723323cfa91d162484b182168489255cd37
LuciSkroder <Luci.skroder@gmail.com>
2025-09-08 09:19:45 +0200

    language selector

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import { NavLink } from "react-router";
+import LanguageSelector from "./LanguageSelector";
+import { useTranslation } from "react-i18next";
 
 export default function NavBar() {
+  const { t } = useTranslation();
   return (
-    <nav>
-      <NavLink to="/cv">CV</NavLink>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
-      <NavLink to="/projects">Projekter</NavLink>
-    </nav>
+    <div>
+      <nav>
+        <NavLink to="/cv">CV</NavLink>
+        <NavLink to="/" end>
+          {t("Home")}
+        </NavLink>
+        <NavLink to="/projects">{t("Projects")}</NavLink>
+      </nav>
+      <LanguageSelector />
+    </div>
   );
 }
