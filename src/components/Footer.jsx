e9065c531bb934345584594246be916f5b89e734
LuciSkroder <Luci.skroder@gmail.com>
2025-09-17 14:28:11 +0200

    footer icon fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,30 +5,42 @@ export default function HeroPhoto() {
   const { t } = useTranslation();
   return (
     <section className="footer">
-      <ul>
-        <li>Luci.skroder@gmail.com</li>
-        <li>+45 25 11 64 32</li>
-      </ul>
+      <div>
+        <h4>{t("contact")}:</h4>
+        <ul>
+          <li>Luci.skroder@gmail.com</li>
+          <li>+45 25 11 64 32</li>
+        </ul>
+      </div>
       <section>
-        <img src="../assets/github.png" alt="github logo" />
-        <img src="../assets/LinkedIn_icon.webp" alt="LinkedIn logo" />
+        <img
+          src="https://luciskroder.github.io/Portfolio/imgs/github.png"
+          alt="github logo"
+        />
+        <img
+          src="https://luciskroder.github.io/Portfolio/imgs/LinkedIn_icon.webp"
+          alt="LinkedIn logo"
+        />
       </section>
-      <ul>
-        <li>
-          <NavLink to="/cv">CV</NavLink>
-        </li>
-        <li>
-          <NavLink to="/" end>
-            {t("Home")}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects">{t("Projects")}</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">{t("About")}</NavLink>
-        </li>
-      </ul>
+      <div>
+        <p>Links:</p>
+        <ul>
+          <li>
+            <NavLink to="/cv">CV</NavLink>
+          </li>
+          <li>
+            <NavLink to="/" end>
+              {t("Home")}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">{t("Projects")}</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">{t("About")}</NavLink>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
