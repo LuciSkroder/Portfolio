e9497c2ae42cc11b1aff1c63a66bf17522f899d9
Luci Gabriella Skrøder <Luci.skroder@gmail.com>
2025-09-19 19:52:09 +0200

    pdf fix

--- src/pages/CVPage.jsx
+++ src/pages/CVPage.jsx
@@ -1,6 +1,16 @@
-import { Document } from 'react-pdf'
+import { Document, Page, pdfjs } from "react-pdf";
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = "/pdfjs/pdf.worker.min.mjs";
 
 export default function CVPage() {
-  return <h1>CV</h1>;
-  <Document file="" />
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', textAlign: "center", margin: "2rem" }}>
+      <Document file="/imgs/cv.pdf">
+        <Page pageNumber={1} />
+        <Page pageNumber={2} />
+      </Document>
+    </div>
+  );
 }
