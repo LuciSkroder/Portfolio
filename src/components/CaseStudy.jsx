33bdca80c871cc77918836aa84168d3236b20681
Luci Gabriella Skrøder <Luci.skroder@gmail.com>
2025-09-19 08:27:00 +0200

    sd

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -8,7 +8,34 @@ export default function CaseStudy() {
     <article>
         <h2 h2>Casestudy -</h2>
         <h2> Steno Museum, {t("steno")}</h2>
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <h3>{t("WBS")}</h3>
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <h3>{t("color")}</h3>
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <h3>{t("responsive")}</h3>
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </article>
-    <aside><img className="stairs" src="https://luciskroder.github.io/Portfolio/imgs/case-stairs.png" alt="Picture of stairs in a forest" /></aside>
+    <aside><img className="stairs" src="https://luciskroder.github.io/Portfolio/imgs/case-stairs.webp" alt="Picture of stairs in a forest" /></aside>
   </section>);
 }
