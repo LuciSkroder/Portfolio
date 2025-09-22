e6266f81eb31b03e486c9aafc64f1eada7d3616a
Luci Gabriella Skrøder <Luci.skroder@gmail.com>
2025-09-22 23:19:39 +0200

    STUFF

---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+import { useTranslation } from "react-i18next";
+
+export default function Projects({ project }) {
+    const { t } = useTranslation();
+
+  return (
+    <section className="project-row">
+        <h2>{t(project.title)}</h2>
+        <a href={project.link} target="_blank">{project.link}</a>
+        <article>
+            <h3>{t("Idid")}</h3>
+            <p>{project.whatIDid}</p>
+         </article>
+        <article>
+            <h3>{t("Ilearn")}</h3>
+            <p>{project.ILearned}</p>
+        </article>
+        <figure>
+            <img src={project.image} alt={`Image of my project: ${project.name}`} />
+        </figure>
+    </section>
+  );
+}
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,3 +1,28 @@
-export default function ProjectsPage() {
-  return <h1>Projekter</h1>;
+import Projects from "../components/Projects";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+export default function ProjectsSection() {
+  const [projects, setProjects] = useState([]);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("/projects.json");
+      const data = await response.json();
+      setProjects(data);
+    }
+    getData();
+  }, []);
+
+  return (
+    <section id="projects">
+      <div className="headline-container">
+        <h1>{t("ProjectsHead")}</h1>
+      </div>
+      {projects.map(project => (
+        <Projects project={project} key={project.id} />
+      ))}
+    </section>
+  );
 }
