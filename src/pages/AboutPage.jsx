8d2fc18896d7c7e05016545df36d4b3225331b54
Luci Gabriella Skrøder <Luci.skroder@gmail.com>
2025-09-23 19:19:14 +0200

    soft skills

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,26 +1,48 @@
+import { useTranslation } from "react-i18next";
+
 export default function AboutPage() {
+  const { t } = useTranslation();
+
   return (
     <section>
       <article>
-        <div>
-          <h2></h2>
-          <img src="https://luciskroder.github.io/Portfolio/imgs/person.png" alt="" />
-        </div>
-        <p></p>
+        <section>
+          <div>
+            <h2>{t("soft-title")}</h2>
+            <img src="https://luciskroder.github.io/Portfolio/imgs/person.png" alt="" />
+          </div>
+          <p>{t("soft-text")}</p>
+        </section>
+        <section>
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </section>
       </article>
       <article>
-        <div>
-          <h2></h2>
-          <img src="https://luciskroder.github.io/Portfolio/imgs/Camera.png" alt="" />
-        </div>
-        <p></p>
+        <section>
+          <div>
+            <h2>{t("photo-title")}</h2>
+            <img src="https://luciskroder.github.io/Portfolio/imgs/Camera.png" alt="" />
+          </div>
+          <p>{t("photo-text")}</p>
+        </section>
+        <section>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </section>
       </article>
       <article>
-        <div>
-          <h2></h2>
-          <img src="https://luciskroder.github.io/Portfolio/imgs/Pen tool.png" alt="" />
-        </div>
-        <p></p>
+        <section>
+          <div>
+            <h2>{t("hobby-title")}</h2>
+            <img src="https://luciskroder.github.io/Portfolio/imgs/Pen tool.png" alt="" />
+          </div>
+          <p>{t("hobby-text")}</p>
+        </section>
       </article>
     </section>
   );
