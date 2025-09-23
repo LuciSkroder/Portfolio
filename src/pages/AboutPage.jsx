a8783d7fd812a1956a9455769d919cf995dd8506
Luci Gabriella Skrøder <Luci.skroder@gmail.com>
2025-09-23 22:34:56 +0200

    bed

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,20 +9,20 @@ export default function AboutPage() {
         <section>
           <div>
             <h2>{t("soft-title")}</h2>
-            <img src="https://luciskroder.github.io/Portfolio/imgs/person.png" alt="" />
+            <img src="https://luciskroder.github.io/Portfolio/imgs/person.png" alt="Small icon of a person" />
           </div>
           <p>{t("soft-text")}</p>
         </section>
         <section>
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src="https://luciskroder.github.io/Portfolio/imgs/softskills-1.png" alt="Diagram of colors corrosponding to certain skills" />
+          <img src="https://luciskroder.github.io/Portfolio/imgs/softskills-2.png" alt="List of skills ranked by percentage" />
         </section>
       </article>
       <article>
         <section>
           <div>
             <h2>{t("photo-title")}</h2>
-            <img src="https://luciskroder.github.io/Portfolio/imgs/Camera.png" alt="" />
+            <img src="https://luciskroder.github.io/Portfolio/imgs/Camera.png" alt="Small icon of a camera" />
           </div>
           <p>{t("photo-text")}</p>
         </section>
@@ -39,7 +39,7 @@ export default function AboutPage() {
         <section>
           <div>
             <h2>{t("hobby-title")}</h2>
-            <img src="https://luciskroder.github.io/Portfolio/imgs/Pen tool.png" alt="" />
+            <img src="https://luciskroder.github.io/Portfolio/imgs/Pen tool.png" alt="Small icon of a cursive pen" />
           </div>
           <p>{t("hobby-text")}</p>
         </section>
